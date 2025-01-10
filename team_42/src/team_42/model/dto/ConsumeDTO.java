fd600cd40592ec0288bc6e1463ecd07b0713ae62
seulgi2027 <seulgi2027@dgu.ac.kr>
2025-01-10 11:46:17 +0900

    Refactor 🚧 : DTO add Lombok

--- team_42/src/team_42/model/dto/ConsumeDTO.java
+++ team_42/src/team_42/model/dto/ConsumeDTO.java
@@ -2,6 +2,15 @@ package team_42.model.dto;
 
 import java.sql.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class ConsumeDTO  {
     private String id;
     private String industry;
@@ -10,74 +19,4 @@ public class ConsumeDTO  {
     private int timeLine;
     private int usageCnt;
     private Date registerDate;
-    
-    public ConsumeDTO() {}
-
-    public ConsumeDTO(String id, String industry, Date transactionDate, int totalAmount, int timeLine, int usageCnt, Date registerDate) {
-        this.id = id;
-        this.industry = industry;
-        this.transactionDate = transactionDate;
-        this.totalAmount = totalAmount;
-        this.timeLine = timeLine;
-        this.usageCnt = usageCnt;
-        this.registerDate = registerDate;
-        
-    }
-
-    
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getIndustry() {
-        return industry;
-    }
-
-    public void setIndustry(String industry) {
-        this.industry = industry;
-    }
-
-    public Date getTransactionDate() {
-        return transactionDate;
-    }
-
-    public void setTransactionDate(Date transactionDate) {
-        this.transactionDate = transactionDate;
-    }
-
-    public int getTotalAmount() {
-        return totalAmount;
-    }
-
-    public void setTotalAmount(int totalAmount) {
-        this.totalAmount = totalAmount;
-    }
-
-    public int getTimeLine() {
-        return timeLine;
-    }
-
-    public void setTimeLine(int timeLine) {
-        this.timeLine = timeLine;
-    }
-
-    public int getUsageCnt() {
-        return usageCnt;
-    }
-
-    public void setUsageCnt(int usageCnt) {
-        this.usageCnt = usageCnt;
-    }
-
-    public Date getRegisterDate() {
-        return registerDate;
-    }
-
-    public void setRegisterDate(Date registerDate) {
-        this.registerDate = registerDate;
-    }
 }
