481d98ddfb9b4765e0f8a828c7a284622b5e8ed6
asdf123124 <eunjunlee98@naver.com>
2025-01-10 14:15:22 +0900

    Feat ➕ view

--- team_42/src/team_42/view/EndView.java
+++ team_42/src/team_42/view/EndView.java
@@ -1,10 +1,7 @@
 package team_42.view;
 
 public class EndView {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public void displayEndMessage(String message) {
+        System.out.println(message);
+    }
 }
--- team_42/src/team_42/view/StartView.java
+++ team_42/src/team_42/view/StartView.java
@@ -1,10 +1,7 @@
 package team_42.view;
 
 public class StartView {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public void displayStartMessage(String message) {
+        System.out.println(message);
+    }
 }
