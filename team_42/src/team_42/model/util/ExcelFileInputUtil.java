af23b9f250b51080c1bd74a8b8296841396ddad6
seulgi2027 <seulgi2027@dgu.ac.kr>
2025-01-10 11:51:55 +0900

    Feat ➕ : for문 추가

--- team_42/src/team_42/model/util/ExcelFileInputUtil.java
+++ team_42/src/team_42/model/util/ExcelFileInputUtil.java
@@ -8,12 +8,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Test;
+import team_42.model.dto.ConsumeDTO;
 
 public class ExcelFileInputUtil {
 	
 	// 파일 읽어오기
 	public void readCSV() {
+		ConsumeDTO consumeDto = new ConsumeDTO();
+		
 		try {
 			File file = new File("C:\\\\01.lab\\\\FISA_4LEE\\\\team_42\\\\card_consume_pattern.csv");
 			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "EUC-KR"));
@@ -24,6 +26,16 @@ public class ExcelFileInputUtil {
 				List<String> aLine = new ArrayList<String>();
 				String[] lineArr = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)",-1);
 				aLine = Arrays.asList(lineArr);
+				
+				for (int i=0; i<aLine.size(); i++) {
+					if (i == 0) {
+						consumeDto.setIndustry(aLine.get(i));
+					}
+					if (i == 1) {
+						//consumeDto.setTransactionDate(new Date(aLine.get(i)));
+					}
+				}
+				
 				System.out.println(aLine);
 			}
 		} catch (Exception e) {
