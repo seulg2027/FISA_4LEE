6f219cf574f35e4b869103e21751785368002a3f
seulgi2027 <seulgi2027@dgu.ac.kr>
2025-01-10 14:40:09 +0900

    Feat ➕ : ExcelUtil Done, DTO re develop

--- team_42/src/team_42/model/util/ExcelFileInputUtil.java
+++ team_42/src/team_42/model/util/ExcelFileInputUtil.java
@@ -4,15 +4,16 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
+import java.sql.Date;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Date;
 
 import org.junit.Test;
 
+import team_42.model.ConsumeDAO;
 import team_42.model.dto.ConsumeDTO;
 
 
@@ -24,49 +25,38 @@ public class ExcelFileInputUtil {
 		ConsumeDTO consumeDto = new ConsumeDTO();
 		
 		try {
-			File file = new File("C:\\01.lab\\01.java\\team_42\\card_consume_pattern.csv");
+			File file = new File("C:\\01.lab\\FISA_4LEE\\team_42\\card_consume_pattern.csv");
 			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "EUC-KR"));
 			
-			String line = "";
+			String line = br.readLine();
 			
 			while ((line = br.readLine())!=null) {
 				List<String> aLine = new ArrayList<String>();
 				String[] lineArr = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)",-1);
 				aLine = Arrays.asList(lineArr);
 				
-				for (int i=0; i<aLine.size(); i++) {
-					if (i == 0) {
-						consumeDto.setIndustry(aLine.get(i));
-					}
-					if (i == 1) {
-//						consumeDto.setTransactionDate(new Date(aLine.get(i)));
-					}
-
-					String industry = aLine.get(0);
-					
-					String dateStr = aLine.get(1);
-					SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
-					Date transaction_date = null;
-					transaction_date = formatter.parse(dateStr);
-					
-					int total_amount = Integer.parseInt(aLine.get(4));
-					
-					// time -> 변환 // time_line
-					int timeInt = Integer.parseInt(aLine.get(2));
-					if(timeInt == 01) {
-						
-					}
-					
-					
-					int usage_cnt = Integer.parseInt(aLine.get(5));
-					
-					LocalDate register_date = LocalDate.now();
-					
-//					industry, transaction_date, total_amount, time_line, usage_cnt, register_date
-					
-				}
+				String industry = aLine.get(0).replace("\"", "");
+				String dateStr = aLine.get(1).replace("\"", "");
+				int timeLine = Integer.parseInt(aLine.get(2).replace("\"", ""));
+				double totalAmount = Double.parseDouble(aLine.get(4).replace("\"", ""));
+				int usageCnt = Integer.parseInt(aLine.get(5).replace("\"", ""));
+				LocalDate registerDate = LocalDate.now();
 				
-				System.out.println(aLine);
+				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+				LocalDate date = LocalDate.parse(dateStr, formatter);
+				
+				consumeDto.setIndustry(industry);
+				consumeDto.setTransactionDate(Date.valueOf(date));
+				consumeDto.setTimeLine(timeLine);
+				consumeDto.setTotalAmount(totalAmount);
+				consumeDto.setUsageCnt(usageCnt);
+				consumeDto.setRegisterDate(Date.valueOf(registerDate));
+
+				System.out.println(consumeDto.toString());
+				
+				ConsumeDAO.createRecord(consumeDto);
+				
+				//System.out.println(aLine);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
