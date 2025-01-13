14824b4efdf5d7bb3f91c51d3102ae4cd9c12d82
seulgi2027 <seulgi2027@dgu.ac.kr>
2025-01-13 09:00:51 +0900

    Feat ➕ : add rest api controller

--- team_42/src/main/team_42/controller/ConsumeController.java
+++ team_42/src/main/team_42/controller/ConsumeController.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import main.team_42.model.ConsumeDAO;
@@ -17,9 +20,19 @@ public class ConsumeController {
 	 * 모든 데이터 조회
 	 */
 	@GetMapping("/getall")
-	public static ResponseEntity getAllCardConsume() throws SQLException {
+	public ResponseEntity getAllCardConsume(@RequestBody String req) throws SQLException {
 		List<ConsumeDTO> list = ConsumeDAO.readAllRecords();
-		System.out.println(list.toString());
+		System.out.println(req);
 		return new ResponseEntity<>(list, HttpStatus.OK);
+	}
+	
+	/*
+	 * 생성
+	 */
+	@PostMapping("/create")
+	public ResponseEntity getCategoryCardConsume(@RequestParam ConsumeDTO consumeDto) throws Exception {
+		boolean result = ConsumeDAO.createRecord(consumeDto);
+		
+		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 }
