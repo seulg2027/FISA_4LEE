de4ac2b291cc8f427f85eaf17888b336ea88b326
asdf123124 <eunjunlee98@naver.com>
2025-01-13 17:13:32 +0900

    Refactor 🚧

--- team_42/src/main/team_42/model/ConsumeDAO.java
+++ team_42/src/main/team_42/model/ConsumeDAO.java
@@ -1,116 +1,115 @@
 package main.team_42.model;
 
-//?
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import main.team_42.model.dto.ConsumeDTO;
 import main.team_42.model.util.DriverUtil;
 
 public class ConsumeDAO {
 
-    // 'Create' 기능
-    public static boolean createRecord(ConsumeDTO consumeDto) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
+	// 'Create' 기능
+	public static boolean createRecord(ConsumeDTO consumeDto) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
 
-        try {
-            con = DriverUtil.getConnection();
-            String sql = "INSERT INTO card_consume (industry, transaction_date, total_amount, time_line, usage_cnt, register_date) VALUES (?, ?, ?, ?, ?, ?)";
-            pstmt = con.prepareStatement(sql);
+		try {
+			con = DriverUtil.getConnection();
+			String sql = "INSERT INTO card_consume (industry, transaction_date, total_amount, time_line, usage_cnt, register_date) VALUES (?, ?, ?, ?, ?, ?)";
+			pstmt = con.prepareStatement(sql);
 
-            pstmt.setString(1, consumeDto.getIndustry()); 
-            pstmt.setDate(2, consumeDto.getTransactionDate());
-            pstmt.setDouble(3, consumeDto.getTotalAmount()); 
-            pstmt.setInt(4, consumeDto.getTimeLine()); 
-            pstmt.setInt(5, consumeDto.getUsageCnt()); 
-            pstmt.setDate(6, consumeDto.getRegisterDate());
+			pstmt.setString(1, consumeDto.getIndustry());
+			pstmt.setDate(2, consumeDto.getTransactionDate());
+			pstmt.setDouble(3, consumeDto.getTotalAmount());
+			pstmt.setInt(4, consumeDto.getTimeLine());
+			pstmt.setInt(5, consumeDto.getUsageCnt());
+			pstmt.setDate(6, consumeDto.getRegisterDate());
 
-            int result = pstmt.executeUpdate();
-            return result == 1; 
-        } finally {
-        	DriverUtil.close(con, pstmt); 
-        }
-    }
+			int result = pstmt.executeUpdate();
+			if (result == 1) {
+				return true;
+			}
+		} finally {
+			DriverUtil.close(con, pstmt);
+		}
+		return false;
+	}
 
-    // 'Read' 기능
-    public static List<ConsumeDTO> readAllRecords(String params) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        List<ConsumeDTO> records = new ArrayList<>(); 
+	// 'Read' 기능
+	public static List<ConsumeDTO> readAllRecords(String params) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<ConsumeDTO> records = null;
 
-        try {
-            con = DriverUtil.getConnection();
-            String sql = "SELECT * FROM card_consume " + params;
-            
-            pstmt = con.prepareStatement(sql);
-            rs = pstmt.executeQuery();
+		try {
+			con = DriverUtil.getConnection();
+			String sql = "SELECT * FROM card_consume " + params;
 
-            while (rs.next()) {
-                records.add(new ConsumeDTO(
-                    rs.getString("id"), 
-                    rs.getString("industry"), 
-                    rs.getDate("transaction_date"), 
-                    rs.getDouble("total_amount"), 
-                    rs.getInt("time_line"), 
-                    rs.getInt("usage_cnt"), 
-                    rs.getDate("register_date") 
-                ));
-            }
-        } finally {
-        	DriverUtil.close(con, pstmt, rs); 
-        }
-        return records; 
-    }
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			records = new ArrayList<>();
+			while (rs.next()) {
+				records.add(new ConsumeDTO(rs.getString("id"), rs.getString("industry"), rs.getDate("transaction_date"),
+						rs.getDouble("total_amount"), rs.getInt("time_line"), rs.getInt("usage_cnt"),
+						rs.getDate("register_date")));
+			}
+		} finally {
+			DriverUtil.close(con, pstmt, rs);
+		}
+		return records;
+	}
 
- // 'Update' 기능
-    public static boolean updateRecord(ConsumeDTO consumeDto) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
+	// 'Update' 기능
+	public static boolean updateRecord(ConsumeDTO consumeDto) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
 
-        try {
-            con = DriverUtil.getConnection();
-            String sql = "UPDATE card_consume SET industry = ?, total_amount = ?, time_line = ?, usage_cnt = ?, register_date = ? WHERE id = ?";
-            pstmt = con.prepareStatement(sql);
+		try {
+			con = DriverUtil.getConnection();
+			String sql = "UPDATE card_consume SET industry = ?, total_amount = ?, time_line = ?, usage_cnt = ?, register_date = ? WHERE id = ?";
+			pstmt = con.prepareStatement(sql);
 
-            pstmt.setString(1, consumeDto.getIndustry()); 
-            pstmt.setDouble(2, consumeDto.getTotalAmount());
-            pstmt.setInt(3, consumeDto.getTimeLine()); 
-            pstmt.setInt(4, consumeDto.getUsageCnt());
-            pstmt.setDate(5, consumeDto.getRegisterDate()); 
-            pstmt.setString(6, consumeDto.getId());
-//          pstmt.setDate(7, consumeDto.getTransactionDate());
+			pstmt.setString(1, consumeDto.getIndustry());
+			pstmt.setDouble(2, consumeDto.getTotalAmount());
+			pstmt.setInt(3, consumeDto.getTimeLine());
+			pstmt.setInt(4, consumeDto.getUsageCnt());
+			pstmt.setDate(5, consumeDto.getRegisterDate());
+			pstmt.setString(6, consumeDto.getId());
 
-            int result = pstmt.executeUpdate();
-            return result == 1; 
-        } finally {
-            DriverUtil.close(con, pstmt); 
-        }
-    }
+			int result = pstmt.executeUpdate();
+			if (result == 1) {
+				return true;
+			}
+		} finally {
+			DriverUtil.close(con, pstmt);
+		}
+		return false;
+	}
 
-    // 'Delete' 기능
-    public static boolean deleteRecord(String id) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
+	// 'Delete' 기능
+	public static boolean deleteRecord(String id) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
 
-        try {
-            con = DriverUtil.getConnection();
-            String sql = "DELETE FROM card_consume WHERE id = ?";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, id); 
+		try {
+			con = DriverUtil.getConnection();
+			String sql = "DELETE FROM card_consume WHERE id = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
 
-            int result = pstmt.executeUpdate();
-            return result == 1; 
-        } finally {
-        	DriverUtil.close(con, pstmt); 
-        }
-    }
-
+			int result = pstmt.executeUpdate();
+			if (result == 1) {
+				return true;
+			}
+		} finally {
+			DriverUtil.close(con, pstmt);
+		}
+		return false;
+	}
 
 }
